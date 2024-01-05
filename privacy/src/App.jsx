1370f10e0e65044ac76a5a9fb5350bd120d7dc98
nGhart <naaghart@gmail.com>
2024-01-05 17:47:32 +0000

    commit

---
+++ privacy/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Privacy Policy</h1>
+        <h4>Effective date: January 4, 2023</h4>
+        <p>
+          This privacy policy notice for Cookiez beads N more ("us", "we", or
+          "our") operates the <a href="https://order-list-vert.vercel.app/"></a>{" "}
+          and how we might collect, store, use and/or share ("process") your
+          information when you use our (the "Services").
+        </p>
+        <p>
+          <strong>Questions and concerns?</strong> Reading this privacy notice
+          will help you understand your privacy rights and choices. If you do
+          not agree with our policies and practices, please do not use our
+          Services. If you still have any questions contact us at{" "}
+          <a href="mailto:naaghwork@gmail.com">naaghwork@gmail.com</a>.
+        </p>
+
+        <h3>WHAT INFORMATION DO WE COLLECT</h3>
+        <p>We collect </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
